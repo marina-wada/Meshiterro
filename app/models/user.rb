2b413b977ab962f4d62071fbbb1c51f2d2cc46ca
marina-wada <wdmrna4i1@gmail.com>
2021-05-17 14:50:28 +0000

    [Update]4-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has many :pst_images, dependent: :destroy
+  has_many :pst_images, dependent: :destroy
 end
