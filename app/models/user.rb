61a2eafd8bb0d34e9dd203a804c10ebca0e9ae91
marina-wada <wdmrna4i1@gmail.com>
2021-05-17 13:01:40 +0000

    [Start]4-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has many :pst_images, dependent: :destroy
 end
